5fff25065de3e91d386949a2508996161e4bc98f
Muhammad Aziz <azizshotokanryu212@gmail.com>
2017-12-09 14:24:25 +0700

    ProviderList

--- app/src/main/java/coder/service/go/com/goservice/ProviderDetailActivity.java
+++ app/src/main/java/coder/service/go/com/goservice/ProviderDetailActivity.java
@@ -5,26 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class ProviderDetailActivity extends AppCompatActivity {
 
     de.hdodenhof.circleimageview.CircleImageView Circle;
+    CheckBox check1,check2,check3;
     TextView tv1;
     TextView tv2;
-    String[]Listnama={"Service Kulkas","Service TV","Service Kipas","Service Mesin Cuci",
-            "Service Playstation","Service Kipas Angin","Service Rice Coocker",
-            "Service AC","Service Sound System"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_detail);
-
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Listnama);
-        ListView listview = (ListView) findViewById(R.id.Listview);
-        listview.setAdapter(adapter);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);// untuk toolbar
         getSupportActionBar().setDisplayShowHomeEnabled(true);// untuk toolbar
