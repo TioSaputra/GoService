158957b7646b762b37633f31470b8dc6a23854de
tio <dev.tio.saputra@gmail.com>
2017-12-10 20:11:37 +0700

    Memberi link kepada semua activity

--- app/src/main/java/coder/service/go/com/goservice/HomeActivity.java
+++ app/src/main/java/coder/service/go/com/goservice/HomeActivity.java
@@ -1,7 +1,9 @@
 package coder.service.go.com.goservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void kategoriA(View view) {
+        Intent i = new Intent(HomeActivity.this, ProviderListActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/coder/service/go/com/goservice/LoginActivity.java
+++ app/src/main/java/coder/service/go/com/goservice/LoginActivity.java
@@ -1,7 +1,9 @@
 package coder.service.go.com.goservice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by tio on 12/10/17.
@@ -13,5 +15,15 @@ public class LoginActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(i);
+    }
+
+    public void signUp(View view) {
+        Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(i);
     }
 }
