0df040f3005f9d62c65b7e57f4bd6a54c25dce34
tio <dev.tio.saputra@gmail.com>
2017-12-10 20:35:17 +0700

    Remake

--- app/src/main/java/coder/service/go/com/goservice/LoginActivity.java
+++ app/src/main/java/coder/service/go/com/goservice/LoginActivity.java
@@ -3,13 +3,14 @@ package coder.service.go.com.goservice;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 /**
  * Created by tio on 12/10/17.
  */
 
-public class LoginActivity extends Activity{
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
