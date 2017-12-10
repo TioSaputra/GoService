9c45d8e1eca361677f4af78bfdfc210356c129e0
tio <dev.tio.saputra@gmail.com>
2017-12-10 19:45:46 +0700

    Nambah Login Activity

--- app/src/main/java/coder/service/go/com/goservice/LoginActivity.java
+++ app/src/main/java/coder/service/go/com/goservice/LoginActivity.java
@@ -1,8 +1,17 @@
 package coder.service.go.com.goservice;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by tio on 12/10/17.
  */
 
-public class LoginActivity {
+public class LoginActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
